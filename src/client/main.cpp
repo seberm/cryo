349894973c0e9139b6207bce512a7015b0e520e0
Vaclav Stransky <jeffy105@gmail.com>
2013-05-30 03:58:21 +0000

    modre pozadi

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -3,22 +3,43 @@
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    // Create the main window
+    sf::Window window(sf::VideoMode(640, 480, 32), "SFML Window", sf::Style::Default, sf::ContextSettings(32));
 
+    // Set the color and depth clear values
     glClearDepth(1.f);
-    glClearColor(0.f, 0.f, 0.f, 0.f);
+    glClearColor(0.f, 0.f, 1.f, 0.f);
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
+        // Process events
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
+            // Close window : exit
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
+
+            // Escape key : exit
+            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
+                window.close();
+
+            // Resize event : adjust viewport
+            if (event.type == sf::Event::Resized)
+                glViewport(0, 0, event.size.width, event.size.height);
+       }
+
+        // Activate the window before using OpenGL commands.
+        // This is useless here because we have only one window which is
+        // always the active one, but don't forget it if you use multiple windows
+        window.setActive();
+
+        // Clear color and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Finally, display the rendered frame on screen
+        window.display();
     }
 
     return 0;
